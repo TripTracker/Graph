2efbd0c172ffb6b97e40c7006142aa660f802708
evtolley@gmail.com <evtolley@gmail.com>
2022-04-05 13:15:38 -0700

    more logg and try

--- src/data-sources/trip-client.ts
+++ src/data-sources/trip-client.ts
@@ -14,8 +14,10 @@ export class TripClient extends RESTDataSource<ExpressContext> {
 
     public async fetchTrip(tripId: string): Promise<Trip> {
         try {
+            console.log(this.baseURL);
+            console.log(config);
             console.log((this.context as any).customHeaders);
-            return await this.get<Trip>(`${this.baseURL}/trip/${tripId}`, null, { headers: (this.context as any).customHeaders });
+            return await this.get<Trip>(`$https://triptracker-trip-api.herokuapp.com/trip/${tripId}`, null, { headers: (this.context as any).customHeaders });
         } catch(error) {
             console.error(error);
             throw error;
